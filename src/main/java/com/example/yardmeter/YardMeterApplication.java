77f24483f02c2bd9e49060af3f5f3c8cb5beca8f
florianscherl <florianscherl@live.at>
2022-06-22 13:43:31 +0200

    init Commit

---
+++ src/main/java/com/example/yardmeter/YardMeterApplication.java
@@ -0,0 +1,13 @@
+package com.example.yardmeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YardMeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YardMeterApplication.class, args);
+	}
+
+}
